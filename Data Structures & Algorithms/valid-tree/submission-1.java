2838bf7acb852c9ec92478f4fad3d0f442c75948
Manmohan Chauhan <manmohanchauhan777@gmail.com>
2026-08-16 10:11:53 +0530

    Add: valid-tree - submission-1

---
+++ Data Structures & Algorithms/valid-tree/submission-1.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean validTree(int n, int[][] edges) {
+        if(edges.length!=n-1) return false;
+        Set<Integer> visited = new HashSet();
+        Map<Integer,List<Integer>> map = new HashMap();
+        for(int []edge : edges){
+            map.computeIfAbsent(edge[0],k-> new ArrayList()).add(edge[1]);
+            map.computeIfAbsent(edge[1],k-> new ArrayList()).add(edge[0]);
+        }
+        
+        if(!dfs(map,-1,0,visited))
+            return false;
+        
+        return visited.size()==n;
+    }
+    boolean dfs(Map<Integer,List<Integer>> map,int parent, int u, Set<Integer> visited){
+        visited.add(u);
+        for(int v: map.getOrDefault(u, new ArrayList<Integer>())){
+            if(!visited.contains(v)){
+                if(!dfs(map,u,v,visited)){
+                    return false;
+                }
+            }else if(v!= parent){
+                return false;
+            }
+        }
+        return true;
+    }
+}

2c776c77c0b58b8948c3d4cd38a42c18c8b3f2c5
Manmohan Chauhan <manmohanchauhan777@gmail.com>
2026-08-26 12:22:25 +0530

    Add: kth-largest-integer-in-a-stream - submission-0

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-0.java
@@ -0,0 +1,30 @@
+class KthLargest {
+    PriorityQueue<Integer> pq = new PriorityQueue();
+    int k;
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        for(int i = 0; i<nums.length; i++){
+            if(pq.size()<k)
+                pq.add(nums[i]);
+            else{
+                if(pq.peek()<nums[i]){
+                    pq.poll();
+                    pq.add(nums[i]);
+                }
+            }
+        }
+
+    }
+    
+    public int add(int val) {
+         if(pq.size()<k)
+                pq.add(val);
+            else{
+                if(pq.peek() < val){
+                    pq.poll();
+                    pq.add(val);
+                }
+            }
+            return pq.peek();
+    }
+}

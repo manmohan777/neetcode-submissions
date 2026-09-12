f918171758692e4a741a8485aa7a2ff55d9a2cba
Manmohan Chauhan <manmohanchauhan777@gmail.com>
2026-09-12 15:15:24 +0530

    Add: interleaving-string - submission-4

---
+++ Data Structures & Algorithms/interleaving-string/submission-4.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isInterleave(String s1, String s2, String s3) {
+        Map<String,Boolean> memo = new HashMap();
+        return solve(s1,s2,s3,0,0,0,memo);
+    }
+    boolean solve(String s1, String s2, String s3, int i, int j, int k, Map<String,Boolean> memo){
+        if(k ==s3.length())
+            return (i==s1.length() && j==s2.length());
+        String key = i+" "+j+" "+k;
+        if(memo.containsKey(key)) return memo.get(key);
+        if(i<s1.length() && s1.charAt(i)==s3.charAt(k)){
+            if( solve(s1,s2,s3,i+1,j,k+1,memo)){
+                memo.put(key,true);
+                return true;
+            }
+        }
+        if(j<s2.length() && s2.charAt(j)==s3.charAt(k)){
+            if( solve(s1,s2,s3,i,j+1,k+1,memo)){
+                memo.put(key,true);
+                return true;
+            }
+        }
+        memo.put(key,false);
+        return false;
+    }
+}
